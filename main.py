9e60391ee42e4c88ac52a54b24dc332741668465
PierreGintrand <pierre.gintrand@supelec.fr>
2017-07-18 16:36:17 +0900

    add results, add pictures..

--- main.py
+++ main.py
@@ -4,15 +4,11 @@ main.py
 @author: Pierre
 """
 
-
 import FeatureDetector
-
 
 filename = 'pic/lena.jpg'
 
-
 numberOfCorners = 50
-
 
 lenaFeatures = FeatureDetector.FeatureDetector(filename, numberOfCorners)
 lenaFeatures.getHarrisCorners()
